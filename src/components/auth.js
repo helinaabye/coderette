ab8e43a5e5552828786c22fc5d859004e5185a35
Helina Abye <helinaago@gmail.com>
2023-10-10 18:21:01 +0300

    initial files and firebase auth

---
+++ src/components/auth.js
@@ -0,0 +1,48 @@
+import { createUserWithEmailAndPassword,signInWithPopup, signOut } from 'firebase/auth';
+import { auth, googleProvider } from '../config/firebase';
+import { useState } from 'react';
+
+export const Auth = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    // console.log(auth?.currentUser?.email)
+    // console.log(auth?.currentUser?.photoURL)
+
+    const signIn = async () => {
+        try {
+            await createUserWithEmailAndPassword(auth, email, password);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, googleProvider);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    const logout = async () => {
+        try {
+            await signOut(auth);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+  return (
+    <div>
+      <input type="text" placeholder="Email..." 
+      onChange={(e) => setEmail(e.target.value)}/>
+      <input type="text" placeholder="Password..." 
+      onChange={(e) => setPassword(e.target.value)}/>
+      <button onClick={signIn}>Sign In</button>
+
+      <button onClick={signInWithGoogle}>Sign In With Google</button>
+
+      <button onClick={logout}>Logout</button>
+    </div>
+  );
+}
---
+++ src/config/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCsMRaPYwXl-TTuntMMLLoGcN-_06ubBo",
+  authDomain: "coderette-f6da7.firebaseapp.com",
+  projectId: "coderette-f6da7",
+  storageBucket: "coderette-f6da7.appspot.com",
+  messagingSenderId: "701438165360",
+  appId: "1:701438165360:web:f71c2c580f4a03cf968fb5",
+  measurementId: "G-TKZ5DL4NJL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
